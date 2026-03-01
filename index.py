e900678caecdb91c5671277d7cfdf08ceef83619
Augustbr01 <augustogatticorrea777@gmail.com>
2026-03-01 16:42:35 -0400

    feat: refactor API to use FastAPI and update search request model; remove unused files

--- index.py
+++ index.py
@@ -1,26 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-import json, os, schedule, time
 from datetime import datetime
-from dotenv import load_dotenv
+from pydantic import BaseModel
 
-import json
-import os
+from fastapi import FastAPI
 
-load_dotenv()
+app = FastAPI()
 
 # ── CONFIGURAÇÃO ──────────────────────────────────────────────
 
-with open('modelosBusca.json', 'r', encoding='utf-8') as arquivo:
-    
-    BUSCAS = json.load(arquivo)
-
-HISTORICO_PATH = "historico.json"
-RESULTADO_PATH = "ofertas.csv"
-
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
 
+class BuscaRequest(BaseModel):
+    nome: str
+    tipo: int | None = None
+    marca: int | None = None
+    string: str | None = None
+    versao: str | None = None
+    faixaano1: int | None = None
+    faixaano2: int | None = None
+    kminicio: str | None = None
+    kmfim: str | None = None
+
 # ── SCRAPER ───────────────────────────────────────────────────
+
+@app.post("/buscar")
+async def buscar(busca: BuscaRequest):
+    busca_dict = busca.model_dump(exclude_none=True)
+
+    lista = buscar_anuncios(busca_dict)
+
+    return {"Total": len(lista), "Resultado: ": lista}
+
+
 def buscar_anuncios(busca: dict):
     params = {k: v for k, v in busca.items() if k != "nome"}  
     resp = requests.get("https://www.shopcar.com.br/busca.php", params=params, headers=HEADERS, timeout=15)
@@ -49,90 +61,91 @@ def buscar_anuncios(busca: dict):
             "link":        link_tag["href"],
             "data_encontrado": datetime.now().strftime("%d/%m/%Y %H:%M"),
         })
+
     return anuncios
 
 # ── HISTÓRICO (evita duplicatas) ──────────────────────────────
-def checar_novos(anuncios):
-    if os.path.exists(HISTORICO_PATH):
-        with open(HISTORICO_PATH) as f:
-            vistos = set(json.load(f))
-    else:
-        vistos = set()
+# def checar_novos(anuncios):
+#     if os.path.exists(HISTORICO_PATH):
+#         with open(HISTORICO_PATH) as f:
+#             vistos = set(json.load(f))
+#     else:
+#         vistos = set()
 
-    novos = [a for a in anuncios if a["id"] not in vistos]
-    vistos.update(a["id"] for a in novos)
+#     novos = [a for a in anuncios if a["id"] not in vistos]
+#     vistos.update(a["id"] for a in novos)
 
-    with open(HISTORICO_PATH, "w") as f:
-        json.dump(list(vistos), f)
-    return novos
+#     with open(HISTORICO_PATH, "w") as f:
+#         json.dump(list(vistos), f)
+#     return novos
 
 # ── EXPORT CSV ────────────────────────────────────────────────
-def salvar_csv(novos):
-    import csv
-    existe = os.path.exists(RESULTADO_PATH)
-    with open(RESULTADO_PATH, "a", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=novos[0].keys())
-        if not existe:
-            writer.writeheader()
-        writer.writerows(novos)
-    print(f"[✓] {len(novos)} novos anúncios salvos em {RESULTADO_PATH}")
+# def salvar_csv(novos):
+#     import csv
+#     existe = os.path.exists(RESULTADO_PATH)
+#     with open(RESULTADO_PATH, "a", newline="", encoding="utf-8") as f:
+#         writer = csv.DictWriter(f, fieldnames=novos[0].keys())
+#         if not existe:
+#             writer.writeheader()
+#         writer.writerows(novos)
+#     print(f"[✓] {len(novos)} novos anúncios salvos em {RESULTADO_PATH}")
 
 # ── WHATSAPP via WPPConnect Server API (EXTERNA) ─────────────────────────
-def notificar_whatsapp(novos, nome_busca):
-    linhas = []
-    for a in novos:
-        linhas.append(
-            f"📌 {a['modelo']} | {a['ano']}\n"
-            f"🎨 {a['cor']} | ⛽ {a['combustivel']}\n"
-            f"📍 {a['km']} | 💰 {a['preco']}\n"
-            f"📍 {a['cidade']}\n"
-            f"🔗 {a['link']}"
-        )
+# def notificar_whatsapp(novos, nome_busca):
+#     linhas = []
+#     for a in novos:
+#         linhas.append(
+#             f"📌 {a['modelo']} | {a['ano']}\n"
+#             f"🎨 {a['cor']} | ⛽ {a['combustivel']}\n"
+#             f"📍 {a['km']} | 💰 {a['preco']}\n"
+#             f"📍 {a['cidade']}\n"
+#             f"🔗 {a['link']}"
+#         )
 
-    msg = f"🔔 *{nome_busca}*\n{len(novos)} novo(s) anuncio(s) encontrado(s):\n\n" + "\n\n--------------------------\n\n".join(linhas)
+#     msg = f"🔔 *{nome_busca}*\n{len(novos)} novo(s) anuncio(s) encontrado(s):\n\n" + "\n\n--------------------------\n\n".join(linhas)
 
-    url = f"{os.environ.get('WPPCONNECT_URL')}/api/{os.environ.get('WPPCONNECT_SESSION')}/send-message"
-    headers = {
-        "Authorization": f"Bearer {os.environ.get('WPPCONNECT_TOKEN')}",
-        "Content-Type": "application/json"
-    }
-    destinatarios = [
-        os.environ.get("DEST_PHONE1"),
-        os.environ.get("DEST_PHONE2"),
-     ]
+#     url = f"{os.environ.get('WPPCONNECT_URL')}/api/{os.environ.get('WPPCONNECT_SESSION')}/send-message"
+#     headers = {
+#         "Authorization": f"Bearer {os.environ.get('WPPCONNECT_TOKEN')}",
+#         "Content-Type": "application/json"
+#     }
+#     destinatarios = [
+#         os.environ.get("DEST_PHONE1"),
+#         os.environ.get("DEST_PHONE2"),
+#      ]
 
-    for numero in destinatarios:
-        payload = {
-            "phone": numero,
-            "isGroup": False,
-            "message": msg
-        }
-        resp = requests.post(url, json=payload, headers=headers, timeout=10)
-        print(f"  [WPP] {numero} → {resp.status_code} | {resp.json().get('message', '')}")
-        time.sleep(5)
+#     for numero in destinatarios:
+#         payload = {
+#             "phone": numero,
+#             "isGroup": False,
+#             "message": msg
+#         }
+#         resp = requests.post(url, json=payload, headers=headers, timeout=10)
+#         print(f"  [WPP] {numero} → {resp.status_code} | {resp.json().get('message', '')}")
+#         time.sleep(5)
 
 
 # ── JOB DIÁRIO ────────────────────────────────────────────────
-def job():
-    print(f"[{datetime.now()}] Iniciando busca em {len(BUSCAS)} modelos...")
-    for busca in BUSCAS:
-        print(f"  → Buscando: {busca['nome']}")
-        try:
-            anuncios = buscar_anuncios(busca)
-            novos = checar_novos(anuncios)
-            if novos:
-                salvar_csv(novos)
-                notificar_whatsapp(novos, busca["nome"])  
-            else:
-                print(f"     Nenhum novo anúncio.")
-        except Exception as e:
-            print(f"     [ERRO] {e}")
-        time.sleep(10)
+#def job():
+#    print(f"[{datetime.now()}] Iniciando busca em {len(BUSCAS)} modelos...")
+    # for busca in BUSCAS:
+    #     print(f"  → Buscando: {busca['nome']}")
+    #     try:
+    #         anuncios = buscar_anuncios(busca)
+    #         novos = checar_novos(anuncios)
+    #         if novos:
+    #             salvar_csv(novos)
+    #             notificar_whatsapp(novos, busca["nome"])  
+    #         else:
+    #             print(f"     Nenhum novo anúncio.")
+    #     except Exception as e:
+    #         print(f"     [ERRO] {e}")
+    #     time.sleep(10)
 
 # ── AGENDAMENTO ───────────────────────────────────────────────
-if __name__ == "__main__":
-    job()  
-    schedule.every(1).hours.do(job) ## Aqui define o tempo de intervalo de cada verificação
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+# if __name__ == "__main__":
+#     job()  
+#     schedule.every(1).hours.do(job) ## Aqui define o tempo de intervalo de cada verificação
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(60)
