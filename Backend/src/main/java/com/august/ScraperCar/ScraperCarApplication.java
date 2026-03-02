b54098b9b7eff91fc332174ec0c6815a8536e2a4
Augustbr01 <augustogatticorrea777@gmail.com>
2026-03-02 19:27:14 -0400

    feat: spring folder structure

---
+++ Backend/src/main/java/com/august/ScraperCar/ScraperCarApplication.java
@@ -0,0 +1,13 @@
+package com.august.ScraperCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraperCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraperCarApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/august/ScraperCar/ScraperCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.august.ScraperCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
