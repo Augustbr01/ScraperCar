9406291a170591e827ffedfd2e08bdaba542f6eb
Augustbr01 <augustogatticorrea777@gmail.com>
2026-03-02 23:33:26 -0400

    feat: create UserModel

--- Backend/src/main/java/com/august/ScraperCar/model/UserModel.java
+++ Backend/src/main/java/com/august/ScraperCar/model/UserModel.java
@@ -1,4 +1,67 @@
 package com.august.ScraperCar.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+import java.util.UUID;
+
+@Entity
 public class UserModel {
+    @Id
+    private UUID id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String numero;
+    private String datanasc;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDatanasc() {
+        return datanasc;
+    }
+
+    public void setDatanasc(String datanasc) {
+        this.datanasc = datanasc;
+    }
 }
