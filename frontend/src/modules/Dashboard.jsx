0a6cafecb5141bdf79f29bad161f1b4539e64fe9
Augustbr01 <augustogatticorrea777@gmail.com>
2026-03-25 14:39:57 -0400

    Starting build frontend application

---
+++ frontend/src/modules/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+function Dashboard() {
+    return (
+        <section className="">
+            <Navbar />
+            
+        </section>
+    )
+}
+
+export default Dashboard
