bcff4385f8c016b50706c70f986fe3df0af8d6d8
Augusto Corrêa <augustogatticorrea777@gmail.com>
2026-03-26 18:03:54 -0400

    Configures and integrates CORS with Spring Security.

--- Backend/src/main/java/com/august/ScraperCar/config/SecurityConfig.java
+++ Backend/src/main/java/com/august/ScraperCar/config/SecurityConfig.java
@@ -1,4 +1,6 @@
 package com.august.ScraperCar.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 
 @Configuration
@@ -22,10 +25,14 @@ public class SecurityConfig {
     @Value("${security.pepper}")
     private String pepper;
 
+    @Qualifier("corsConfiguration")
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
