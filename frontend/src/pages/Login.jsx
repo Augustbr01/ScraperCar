50d0ac3dbc033a5d7d4fad24171707562a89c9bc
Augusto Corrêa <augustogatticorrea777@gmail.com>
2026-03-26 00:21:32 -0400

    Auth config

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+        <>
+            <div className='bg-[#0A0A0A] min-h-screen overflow-x-hidden relative'>
+                <div className='absolute z-0 -top-120 -right-55 w-200 h-200 bg-[#00FFFF]/30 rounded-full blur-[200px]'></div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Login
